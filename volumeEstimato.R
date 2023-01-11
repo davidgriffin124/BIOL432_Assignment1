70f028934d115f2d5bc076662174cb2234d0e8d1
David Griffin <davidgriffin124@gmail.com>
2023-01-10 19:32:05 -0500

    changed volume column title

--- volumeEstimato.R
+++ volumeEstimato.R
@@ -6,7 +6,7 @@ dat = read.csv("measurements.csv")
 
 # Adds volume column by forumula V = pi*(r^2)*h
 dat2 = dat %>%
-  mutate(Volume = (pi*((Limb_width/2)^2)*Limb_length),
+  mutate(Limb_volume = (pi*((Limb_width/2)^2)*Limb_length),
          .after = Limb_length)
 
 # Updates measurements file to include volume column
